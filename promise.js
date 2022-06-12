15b338e7a5d46d8ddbfc0c968b562ba307917f2b
zakynaufal98 <zakynaufal1998@gmail.com>
2022-06-12 18:07:32 +0700

    feat:Update Challange 07

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    let inputFunc = (await promiseTheaterIXX()).concat(
+      await promiseTheaterVGC()
+    );
+    return inputFunc.filter((item) => item.hasil === emosi).length;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
